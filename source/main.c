2da7fee207eff2a47ac64c108021981ab9389ae2
MysticExile <ethan.baboeram@gmail.com>
2019-01-16 14:20:36 +0100

    Initial-commit

---
+++ source/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <switch.h>
+
+int main(int argc, char **argv) {
+  // Initialize console. Using NULL as the second argument tells the console
+  // library to use the internal console structure as current one.
+  consoleInit(NULL);
+
+  // Move the cursor to row 16 and column 20 and then prints "Hello World!"
+  // To move the cursor you have to print "\x1b[r;cH", where r and c are
+  // respectively the row and column where you want your cursor to move
+  printf("\x1b[16;20HUr mom gay lol");
+
+  while (appletMainLoop()) {
+    // Scan all the inputs. This should be done once for each frame
+    hidScanInput();
+
+    // hidKeysDown returns information about which buttons have been just
+    // pressed (and they weren't in the previous frame)
+    u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+
+    if (kDown & KEY_PLUS)
+      break; // break in order to return to hbmenu
+
+    consoleUpdate(NULL);
+  }
+
+  consoleExit(NULL);
+  return 0;
+}
